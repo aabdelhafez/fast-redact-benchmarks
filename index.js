0224be911851bb51afef80777569745177e7a184
Abdelrahman Abdelhafez <abdelrahman.abdelhafez@trivago.com>
2022-01-07 17:16:00 +0100

    Set up benchmarking code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('fast-redact-benchmarks');
+const Benchmark = require('benchmark');
+
+const suite = new Benchmark.Suite();
+
+suite
+  .add('RegExp#test', function () {
+    /o/.test('Hello World!');
+  })
+  .add('String#indexOf', function () {
+    'Hello World!'.indexOf('o') > -1;
+  })
+  .on('cycle', function (event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run({ async: true });
