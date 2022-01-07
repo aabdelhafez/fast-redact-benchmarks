92d9bc0fdc186b08a3273964b963c79bc0666042
Abdelrahman Abdelhafez <abdelrahman.abdelhafez@trivago.com>
2022-01-07 17:31:53 +0100

    Benchmark fast-redact against JSON.stringify

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const Benchmark = require('benchmark');
+const fastRedact = require('fast-redact');
 
 const suite = new Benchmark.Suite();
 
 suite
-  .add('RegExp#test', function () {
-    /o/.test('Hello World!');
+  .add('jsonStringify', function () {
+    jsonStringify(getDummyObject());
   })
-  .add('String#indexOf', function () {
-    'Hello World!'.indexOf('o') > -1;
+  .add('fastRedactWithSerialization', function () {
+    fastRedactWithSerialization(getDummyObject());
+  })
+  .add('fastRedactWithoutSerialization', function () {
+    fastRedactWithoutSerialization(getDummyObject());
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
@@ -16,3 +20,30 @@ suite
     console.log('Fastest is ' + this.filter('fastest').map('name'));
   })
   .run({ async: true });
+
+function getDummyObject() {
+  return {
+    headers: {
+      host: 'http://example.com',
+      cookie: `oh oh we don't want this exposed in logs in etc.`,
+      referer: `if we're cool maybe we'll even redact this`,
+    },
+  };
+}
+
+function jsonStringify(object) {
+  JSON.stringify(object);
+}
+
+function fastRedactWithSerialization(object) {
+  fastRedact({
+    paths: ['headers.cookie', 'headers.referer'],
+  })(object);
+}
+
+function fastRedactWithoutSerialization(object) {
+  fastRedact({
+    paths: ['headers.cookie', 'headers.referer'],
+    serialize: false,
+  })(object);
+}
